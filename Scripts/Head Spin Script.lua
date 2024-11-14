da9e9fb56f42e9a001fc393d66ebb77def67abf4
JupitersAwesome <Garrett.John.Erich@gmail.com>
2024-11-14 08:53:43 -0500

    Add files via upload

---
+++ Scripts/Head Spin Script.lua
@@ -0,0 +1,6 @@
+for X = 1, math.huge, 0.1 do
+        wait()
+        game.Workspace.Mad5killz.Torso.Neck.C0 = CFrame.new(0,1.5,0) * CFrame.fromAxisAngle(Vector3.new(0,1,0), X)
+        game.Workspace.Mad5killz.Torso.Neck.C1 = CFrame.new(0,0,0)
+end
+
